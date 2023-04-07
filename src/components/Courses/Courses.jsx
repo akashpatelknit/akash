a6edb6e4f5b5c47d0e94f71111c454c5e8520126
akashpatelknit <cotsec14@gmail.com>
2023-04-08 01:08:08 +0530

    image added

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -41,7 +41,10 @@ const Courses = () => {
       <Grid
         templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)']}
         gap={6}
-        marginLeft={['2rem', '6rem']}
+        // marginLeft={['2rem', '3rem','6rem']}
+        width={['250px','800px']}
+       
+        margin={'auto'}
       >
         <GridItem>
           <Link to="/netflix">
@@ -49,6 +52,7 @@ const Courses = () => {
               src="https://i.pinimg.com/originals/25/ae/6c/25ae6c3bbc39e96c2948e863e6fe61c4.gif"
               title="Netflix Clone"
               colour="red"
+              link='/netflix'
             />
           </Link>
         </GridItem>
@@ -58,6 +62,7 @@ const Courses = () => {
               src="https://i.pinimg.com/originals/79/35/fd/7935fdf1fe2c5dd8205de03a214befb0.gif"
               title="Weather App"
               colour="black"
+              link='/weather'
             />
           </Link>
         </GridItem>
@@ -67,6 +72,7 @@ const Courses = () => {
               src="https://cdn.dribbble.com/users/686119/screenshots/2435023/untitled.gif"
               title="My Library"
               colour="black"
+              link='/lib'
             />
           </Link>
         </GridItem>
@@ -76,6 +82,7 @@ const Courses = () => {
               src="https://onix-systems.com/_next/image?url=https%3A%2F%2Fcdn.onix-systems.com%2Fstatic%2Fimages%2FPages%2FFitnessApp%2Frunning_woman.gif&w=640&q=75"
               title="Onthemove"
               colour="black"
+              link='/otm'
             />
           </Link>
         </GridItem>
